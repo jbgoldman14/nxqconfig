6bb1d101300af79472503c660824238abb4ba463
jbgoldman1104 <jbgoldman1104@gmail.com>
2024-04-18 19:13:27 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jbgoldman1104/nxqconfig
+
+go 1.21.1
---
+++ nxqconfig.go
@@ -0,0 +1,14 @@
+package nxqconfig
+
+// NexQloud Config
+const (
+	MaintenanceWallet      = "nxq1edgqqqgajcvs0wxhqda9wzaa5x2l902fu5yxaw"
+	Vault1Wallet	       = "nxq1mg9nzc09dxmpm3e58rt4taef8fz9cy6ek5gvzd"
+	Vault2Wallet	       = "nxq1nrvz9zqyr7lf776lwg6sh0v4cl5x5dxdk84c6e"
+	Vault3Wallet	       = "nxq1f4n5l9aggyjuydd24amxpw6v8cej7d59n7usfg"
+	Vault4Wallet	       = "nxq1l8hexeltx5v36svsdxl8u6zp8fdegq5mczuzdq"
+	Vault5Wallet	       = "nxq17crcer5e5gdy7nnkjp4shs8ach8lj7s8x38s90"
+	ContractAddress        = "0xf9597159591e353f6A391089974321d8ea004098"
+	MinValidatorToken      = 50
+	MinValidatorNFTBalance = 5
+)
