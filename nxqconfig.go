69216386c00e27aede35328a6c99e52cda955006
jbgoldman1104 <jbgoldman1104@gmail.com>
2024-04-23 00:31:49 +0000

    Updated wallets for Mainnet

--- nxqconfig.go
+++ nxqconfig.go
@@ -2,13 +2,21 @@ package nxqconfig
 
 // NexQloud Config
 const (
-	MaintenanceWallet      = "nxq1edgqqqgajcvs0wxhqda9wzaa5x2l902fu5yxaw"
-	Vault1Wallet	       = "nxq1mg9nzc09dxmpm3e58rt4taef8fz9cy6ek5gvzd"
-	Vault2Wallet	       = "nxq1nrvz9zqyr7lf776lwg6sh0v4cl5x5dxdk84c6e"
-	Vault3Wallet	       = "nxq1f4n5l9aggyjuydd24amxpw6v8cej7d59n7usfg"
-	Vault4Wallet	       = "nxq1l8hexeltx5v36svsdxl8u6zp8fdegq5mczuzdq"
-	Vault5Wallet	       = "nxq17crcer5e5gdy7nnkjp4shs8ach8lj7s8x38s90"
+	MaintenanceWallet      = "nxq1mm3avcg58d0ht78syf3uyv30yljmyymy3fugyp"
+	Vault1Wallet	       = "nxq1xfm8n072h6antu94tac0skh42u6vj6hrquqznt"
+	Vault2Wallet	       = "nxq1ptds9mnfns4ufesjga0d0c5a392uw85dpe2hd6"
+	Vault3Wallet	       = "nxq1cmys66z7m5pv3ygcpm9y5hsyr8m65at6rqnhtk"
+	Vault4Wallet	       = "nxq1cylpqf2ssa8ajtaledychymmsmc5v8zw3kex26"
+	Vault5Wallet	       = "nxq1dmnc5utac6yz8tcd6r0hjzke3fgsf6t87yljme"
+	GasCollector		   = "nxq1jdky9yyvucgv7ucepmzrpn9wa4n7qm05t4sa95"
 	ContractAddress        = "0xf9597159591e353f6A391089974321d8ea004098"
 	MinValidatorToken      = 50
 	MinValidatorNFTBalance = 5
 )
+
+
+
+
+
+
+
