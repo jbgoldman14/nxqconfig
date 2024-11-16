cb71c2b814f71c37c57206ad13f57791c22efc39
CryptoWizardTeam <markojokic588@gmail.com>
2024-11-16 20:48:39 +0000

    update-change the wallet addresses

--- nxqconfig.go
+++ nxqconfig.go
@@ -2,13 +2,13 @@ package nxqconfig
 
 // NexQloud Config
 const (
-	MaintenanceWallet      = "nxq1mm3avcg58d0ht78syf3uyv30yljmyymy3fugyp"
-	Vault1Wallet	       = "nxq1xfm8n072h6antu94tac0skh42u6vj6hrquqznt"
-	Vault2Wallet	       = "nxq1ptds9mnfns4ufesjga0d0c5a392uw85dpe2hd6"
-	Vault3Wallet	       = "nxq1cmys66z7m5pv3ygcpm9y5hsyr8m65at6rqnhtk"
-	Vault4Wallet	       = "nxq1cylpqf2ssa8ajtaledychymmsmc5v8zw3kex26"
-	Vault5Wallet	       = "nxq1dmnc5utac6yz8tcd6r0hjzke3fgsf6t87yljme"
-	GasCollector		   = "nxq1jdky9yyvucgv7ucepmzrpn9wa4n7qm05t4sa95"
+	MaintenanceWallet      = "nxq1f76z635pq0l0avtqac4uu8tll0vedx8g28h2kc"
+	Vault1Wallet	       = "nxq1y62xfyv7e2rhpevx8xgdfw535u5axuzyfzxjez"
+	Vault2Wallet	       = "nxq1a2sspecr5hh5q70t9j6putc57f37k5s9gdk9tt"
+	Vault3Wallet	       = "nxq15g3patvhxzl0yzhtjyfpyzzuwkje2y0583pjma"
+	Vault4Wallet	       = "nxq1t4p2upwxqdhxlk96qdvqyk8z0fu024n4z4v6dl"
+	Vault5Wallet	       = "nxq17lrmazk8ek7fx8etcgl06kc0sppfv4yvnvspm0"
+	GasCollector		   = "nxq1usgzaq4gmgm3wwmlhw7lhwfkcpqr59zp5xmmd4"
 	ContractAddress        = "0x93A8a61E4b6b769005aD02221C0CBdfAF5432CF2"
 	MinValidatorToken      = 1
 	MinValidatorNFTBalance = 5
